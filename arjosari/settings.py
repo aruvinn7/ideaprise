7e938821db3baf02aab70b71ee20a932e999af40
Zepppeli <alvinyunanta68@gmail.com>
2025-08-10 01:21:04 +0800

    csrf

--- arjosari/settings.py
+++ arjosari/settings.py
@@ -135,3 +135,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-a624.up.railway.app'
+]
